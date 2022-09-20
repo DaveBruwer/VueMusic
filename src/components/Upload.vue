03a45a1cbcc859171d6dbd6814ebf2dddfb4f2c6
DaveBruwer <davebruwer@gmail.com>
2022-09-21 09:29:25 +1200

    added firebase persisntance

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -70,6 +70,21 @@ export default {
           return;
         }
 
+        console.log(navigator);
+        console.log(navigator.onLine);
+
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 1000,
+            name: file.name,
+            variant: 'bg-red-400',
+            icon: 'fas fa-times',
+            text_class: 'text-red-400',
+          });
+          return;
+        }
+
         const storageRef = storage.ref(); // music-38185.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`); // music-38185.appspot.com/songs/example.mp3
         const task = songsRef.put(file);
