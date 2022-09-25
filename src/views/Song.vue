a8ad6328348ebb30c9ae44005ecfab7c68845d69
DaveBruwer <davebruwer@gmail.com>
2022-09-25 14:59:17 +1300

    Reformatted song page load.
    Changed the song page to load on beforeRouteEnter instead of created.

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -186,22 +186,27 @@ export default {
       });
     },
   },
-  async created() {
-    const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
+  async beforeRouteEnter(to, from, next) {
+    const docSnapshot = await songsCollection.doc(to.params.id).get();
 
-    if (!docSnapshot.exists) {
-      this.$router.push({ name: 'home' });
-      return;
-    }
+    next(async (vm) => {
+      if (!docSnapshot.exists) {
+        vm.$router.push({ name: 'home' });
+        return;
+      }
 
-    const { sort } = this.$route.query;
+      const { sort } = vm.$route.query;
 
-    this.sort = sort === '1' || sort === '2' ? sort : '1';
+      // eslint-disable-next-line no-param-reassign
+      vm.sort = sort === '1' || sort === '2' ? sort : '1';
 
-    this.song = docSnapshot.data();
-    await this.getComments();
+      // eslint-disable-next-line no-param-reassign
+      vm.song = docSnapshot.data();
+      await vm.getComments();
 
-    this.song.comment_count = this.comments.length;
+      // eslint-disable-next-line no-param-reassign
+      vm.song.comment_count = vm.comments.length;
+    });
   },
 };
 </script>
